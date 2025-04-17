ebf2491d8aeaba79f801e9916d6736aba7789687
jzlpr <jazzlikeproblem@gmail.com>
2025-04-17 14:35:50 +0200

    Splitted models into separate files

--- Shared/Models/Product.cs
+++ Shared/Models/Product.cs
@@ -8,10 +8,4 @@ namespace Shared.Models
         public int Stock { get; set; }
         public Category? Category { get; set; }
     }
-
-    public class Category
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
 }
